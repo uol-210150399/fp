8a43591f728764607b21ab1f24476bc046ccb070
uol-210150399 <203115764+uol-210150399@users.noreply.github.com>
2025-01-03 12:46:43 -0600

    Add survey service

--- backend/src/survey/survey.module.ts
+++ backend/src/survey/survey.module.ts
@@ -1,10 +1,16 @@
+import { SurveyService } from './survey.service';
 import { Module } from '@nestjs/common';
 import { SurveyQueryResolver } from './resolvers/survey-query.resolver';
 import { SurveyMutationResolver } from './resolvers/survey-mutation.resolver';
 import { SurveyResolver } from './survey.resolver';
 
 @Module({
-  providers: [SurveyResolver, SurveyQueryResolver, SurveyMutationResolver],
+  providers: [
+    SurveyService,
+    SurveyResolver,
+    SurveyQueryResolver,
+    SurveyMutationResolver,
+  ],
   exports: [],
 })
 export class SurveyModule {}
